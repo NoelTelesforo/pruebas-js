e4c7be3abe2cb0db75139ddedc361451179f6b97
NoelTelesforo <ntelesforomartinez@gmail.com>
2023-04-26 13:48:32 -0600

    inital commit
    add first practice of js operators

---
+++ operadores/index.js
@@ -0,0 +1,45 @@
+
+
+// Operador de suma (+) suma números:
+// Ejemplos:
+
+let c = 5;
+let d = 2;
+let e = c + d;
+document.getElementById("suma").innerHTML = e;
+
+console.log("resultado de la suma= ", e)
+
+
+
+// Operador de multiplicación (*) multiplica números.
+// Ejemplo:
+
+let f = 3;
+let g = 2;
+let h = f * g;
+
+
+
+// Operadores aritméticos, se utilizan para realizar operaciones aritmeticas con números:
+
+let i = 3;
+let j = (100 + 50) * i;
+
+
+
+// Operadores de asignación, asiganan valores a las variables de JavaScript. El operador de asignacion de adicion (+=) agrega un valor a una variable.
+
+let k = 10;
+k += 5;
+
+
+
+// operador de asignacion  (=) asigna un valor a una variable.
+// Ejemplos:
+
+let a = 10;
+let b = 5;
+
+
+// Operadores de comparacion de JavaScript
